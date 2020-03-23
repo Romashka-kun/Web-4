c70711e9cc9260b868f9cec9e5fdfc0bfb3424e9
Roman <braid98@yandex.ru>
2020-03-23 17:52:22 +0300

    Remove redundant files

--- www/blog2019/iteration2/inc/head_tags.php
+++
@@ -1,7 +0,0 @@
-	<?php
-	  //Общие для всех страниц сайта элементы <head>
-	  //У нас это сейчас статичный HTML, но вполне вероятны и программные решения.
-	?>
-	<link href="style.css" type="text/css" rel="stylesheet" />
-	<script src="https://code.jquery.com/jquery-1.10.2.js" integrity="sha256-it5nQKHTz+34HijZJQkpNBIHsjpV8b6QzMJs9tmOBSo=" crossorigin="anonymous"></script>
-	<script src="script.js" type="text/javascript"></script>
